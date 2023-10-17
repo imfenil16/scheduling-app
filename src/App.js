2965e0bc8e91b624d6e68b1c54d36dc6364ecb5e
fenil <7fenilpatel@gmail.com>
2023-10-17 17:42:57 -0400

    WIP Fix Total Count

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React, { useState } from 'react';
 import './App.css';
+import StaffSelect from './components/staffselect';
+
+const staffMembers = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7'];
+
+const timeSlots = [
+  'Morning UpStairs',
+  'Morning Down Stairs',
+  'Morning Parking Lot',
+  'Lunch A',
+  'Lunch B',
+  'Lunch C',
+  'Lunch D',
+  'Afternoon UpStairs',
+  'Afternoon Down Stairs',
+  'Afternoon Parking Lot',
+];
 
 function App() {
   const [schedule, setSchedule] = useState({});
-  const staffMembers = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7'];
 
   const handleStaffSelect = (day, timeSlot, staff) => {
+    // Prevent consecutive lunch slots
+    const currentDaySchedule = schedule[day] || {};
+    const prevTimeSlot = timeSlots[timeSlots.indexOf(timeSlot) - 1];
+    if (
+      prevTimeSlot &&
+      currentDaySchedule[prevTimeSlot] === staff &&
+      timeSlot.includes('Lunch')
+    ) {
+      alert(
+        'Consecutive lunch slots for the same staff member are not allowed.'
+      );
+      return;
+    }
+
     setSchedule((prevSchedule) => ({
       ...prevSchedule,
       [day]: {
-        ...prevSchedule[day],
+        ...currentDaySchedule,
         [timeSlot]: staff,
       },
     }));
@@ -22,9 +51,20 @@ function App() {
     ).length;
   };
 
+  const calculateTotalLoad = (staff) => {
+    let totalLoad = 0;
+    timeSlots.forEach((timeSlot) => {
+      const daySchedule = schedule[timeSlot] || {};
+      if (daySchedule[staff]) {
+        totalLoad++;
+      }
+    });
+    return totalLoad;
+  };
+
   return (
-    <div className='App'>
-      <h1>Schedule</h1>
+    <div className='container mx-auto p-8'>
+      <h1 className='text-2xl mb-4'>Schedule</h1>
       <table>
         <thead>
           <tr>
@@ -37,33 +77,29 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Morning UpStairs</td>
-            <td>
-              <select
-                value={schedule['Monday']?.['Morning UpStairs'] || ''}
-                onChange={(e) =>
-                  handleStaffSelect(
-                    'Monday',
-                    'Morning UpStairs',
-                    e.target.value
-                  )
-                }
-              >
-                <option value=''>Select Staff</option>
-                {staffMembers.map((staff) => (
-                  <option key={staff} value={staff}>
-                    {staff}
-                  </option>
-                ))}
-              </select>
-            </td>
-          </tr>
+          {timeSlots.map((timeSlot) => (
+            <tr key={timeSlot}>
+              <td>{timeSlot}</td>
+              {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'].map(
+                (day) => (
+                  <td key={day}>
+                    <StaffSelect
+                      value={schedule[day]?.[timeSlot] || ''}
+                      onChange={(e) =>
+                        handleStaffSelect(day, timeSlot, e.target.value)
+                      }
+                      staffMembers={staffMembers}
+                    />
+                  </td>
+                )
+              )}
+            </tr>
+          ))}
         </tbody>
       </table>
 
-      <h2>Load Section</h2>
-      <table>
+      <h2 className='text-2xl my-4'>Load Section</h2>
+      <table className='table-auto border border-collapse w-full'>
         <thead>
           <tr>
             <th>Staff Member</th>
@@ -79,18 +115,12 @@ function App() {
           {staffMembers.map((staff) => (
             <tr key={staff}>
               <td>{staff}</td>
-              <td>{countStaffLoad(staff, 'Monday')}</td>
-              <td>{countStaffLoad(staff, 'Tuesday')}</td>
-              <td>{countStaffLoad(staff, 'Wednesday')}</td>
-              <td>{countStaffLoad(staff, 'Thursday')}</td>
-              <td>{countStaffLoad(staff, 'Friday')}</td>
-              <td>
-                {countStaffLoad(staff, 'Monday') +
-                  countStaffLoad(staff, 'Tuesday') +
-                  countStaffLoad(staff, 'Wednesday') +
-                  countStaffLoad(staff, 'Thursday') +
-                  countStaffLoad(staff, 'Friday')}
-              </td>
+              {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'].map(
+                (day) => (
+                  <td key={day}>{countStaffLoad(staff, day)}</td>
+                )
+              )}
+              <td>{calculateTotalLoad(staff)}</td>
             </tr>
           ))}
         </tbody>
