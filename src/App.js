591641654f0233f810f36d66e4393e028afeeb3b
fenil <7fenilpatel@gmail.com>
2023-10-17 17:34:52 -0400

    Add MVP Select

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [schedule, setSchedule] = useState({});
+
+  const handleStaffSelect = (day, timeSlot, staff) => {
+    setSchedule((prevSchedule) => ({
+      ...prevSchedule,
+      [day]: {
+        ...prevSchedule[day],
+        [timeSlot]: staff,
+      },
+    }));
+  };
+
   return (
     <div className='App'>
       <h1>Schedule</h1>
@@ -19,11 +31,22 @@ function App() {
         <tbody>
           <tr>
             <td>Morning UpStairs</td>
-            <td>X</td>
-            <td>X</td>
-            <td>X</td>
-            <td>X</td>
-            <td>X</td>
+            <td>
+              <select
+                value={schedule['Monday']?.['Morning UpStairs'] || ''}
+                onChange={(e) =>
+                  handleStaffSelect(
+                    'Monday',
+                    'Morning UpStairs',
+                    e.target.value
+                  )
+                }
+              >
+                <option value=''>Select Staff</option>
+                <option value='X1'>X1</option>
+                <option value='X2'>X2</option>
+              </select>
+            </td>
           </tr>
         </tbody>
       </table>
