66ac4c2f2d4b281a1d4fd703bfcff9305761fd18
fenil <7fenilpatel@gmail.com>
2023-10-17 17:35:39 -0400

    Add Load Section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [schedule, setSchedule] = useState({});
+  const staffMembers = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7'];
 
   const handleStaffSelect = (day, timeSlot, staff) => {
     setSchedule((prevSchedule) => ({
@@ -12,6 +13,13 @@ function App() {
         [timeSlot]: staff,
       },
     }));
+  };
+
+  const countStaffLoad = (staff, day) => {
+    const daySchedule = schedule[day] || {};
+    return Object.values(daySchedule).filter(
+      (staffMember) => staffMember === staff
+    ).length;
   };
 
   return (
@@ -43,11 +51,48 @@ function App() {
                 }
               >
                 <option value=''>Select Staff</option>
-                <option value='X1'>X1</option>
-                <option value='X2'>X2</option>
+                {staffMembers.map((staff) => (
+                  <option key={staff} value={staff}>
+                    {staff}
+                  </option>
+                ))}
               </select>
             </td>
           </tr>
+        </tbody>
+      </table>
+
+      <h2>Load Section</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Staff Member</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {staffMembers.map((staff) => (
+            <tr key={staff}>
+              <td>{staff}</td>
+              <td>{countStaffLoad(staff, 'Monday')}</td>
+              <td>{countStaffLoad(staff, 'Tuesday')}</td>
+              <td>{countStaffLoad(staff, 'Wednesday')}</td>
+              <td>{countStaffLoad(staff, 'Thursday')}</td>
+              <td>{countStaffLoad(staff, 'Friday')}</td>
+              <td>
+                {countStaffLoad(staff, 'Monday') +
+                  countStaffLoad(staff, 'Tuesday') +
+                  countStaffLoad(staff, 'Wednesday') +
+                  countStaffLoad(staff, 'Thursday') +
+                  countStaffLoad(staff, 'Friday')}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
